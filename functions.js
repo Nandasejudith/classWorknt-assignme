da172c641dc50b07745537b0bb01ae7316ec395e
Nandasejudith <judithnandase@gmail.com>
2021-03-21 17:30:38 +0300

    nested

---
+++ functions.js
@@ -0,0 +1,11 @@
+function identity(){
+var a = "Judith", b ="Patience";
+
+    function name(){
+var k = a + " and " + b + " are friends "
+console.log (k)
+    }
+name();
+return k
+}
+identity()
---
+++ loops.js
@@ -0,0 +1,7 @@
+function eat(){
+    var book = [34,56,65,67,76];
+    for  (x=2; x<=book.length; x++) {
+        console.log(book[x]);
+    }
+    
+}eat();
